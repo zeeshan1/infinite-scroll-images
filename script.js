c2429dc1c8be6fe5a8eac751a7ba34d39a6c31e7
zeeshan1 <zeeshan.barani@gmail.com>
2022-08-19 22:41:50 +0400

    improved performance and page load

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 const loader = document.getElementById('loader');
-const imageContainer = document.getElementById('image-container')
-let imagesArray = [];
-const YOUR_ACCESS_KEY = ''
-const url = `https://api.unsplash.com/photos/?client_id=${YOUR_ACCESS_KEY}`;
+const imageContainer = document.getElementById('image-container');
+let imageArray = [];
+let count = 5;
+const key = '';
+let url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${count}`;
 loader.hidden = false;
 
+let isLoaded = false;
+let totalIamge = 0;
+let imagesLoaded = 0;
 
+function imageLoaded() {
+    imagesLoaded++
+    if (totalIamge === imagesLoaded) {
+        isloaded = true;
+        count = 30;
+        url = `https://api.unsplash.com/photos/random?client_id=${key}&count=${count}`;
+    }
+}
 
-async function getimages() {
-    const response = await fetch(url);
-    const content = await response.json();
-    imagesArray = [...imagesArray, ...content]
+async function getImages() {
+    const res = await fetch(url);
+    imageArray = await res.json();
     paintImage();
 }
 
-function paintImage() {
-    let ele = ''
-    let img = ''
-    imagesArray.forEach((item) => {
-        ele = document.createElement('a');
-        img = document.createElement('img');
-        ele.setAttribute('href', item.links.html);
-        img.setAttribute('src', item.urls.regular);
-        img.setAttribute('alt', item.id);
-        ele.appendChild(img);
-        imageContainer.appendChild(ele);
-    })
-    loader.hidden = true;
+function setAttributes(element, attributes) {
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
 }
 
-document.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY > document.body.clientHeight - 1000) {
-        console.log(imagesArray)
+function paintImage() {
+    imagesLoaded = 0;
+    totalIamge = imageArray.length;
+    imageArray.forEach(item => {
+        const link = document.createElement('a');
+        setAttributes(link, {
+            'href': item.links.html,
+            'target': '_blank'
+        })
 
-        getimages();
+        const img = document.createElement('img')
+        setAttributes(img, {
+            'src': item.urls.regular,
+            'alt': item.id
+        })
+        img.addEventListener('load', imageLoaded)
+
+
+        link.appendChild(img);
+        imageContainer.appendChild(link)
+        loader.hidden = true;
+    })
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY > document.body.clientHeight - 1000 && isloaded) {
+        getImages();
     }
-
 })
 
-getimages();
+getImages();
